2485c91bdfcb10f3a516e15d74395145328d7a62
oborskyivitalii <oborskyivitalii@gmail.com>
2026-08-03 21:13:11 +0200

    Initialize repository validator package

---
+++ tools/repository_validator/__init__.py
@@ -0,0 +1,5 @@
+"""Executable repository contract validator."""
+
+from .validator import Issue, RepositoryValidator, validate_repository
+
+__all__ = ["Issue", "RepositoryValidator", "validate_repository"]
